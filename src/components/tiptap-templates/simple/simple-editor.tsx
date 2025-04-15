28b039b854eed41732ecffcd2ca76ca0771aaf44
peter <peter@soulware.kr>
2025-04-15 16:41:40 +0900

    refactor: remove tiptap's subscript/superscript features and clean up unused code/dependencies

--- src/components/tiptap-templates/simple/simple-editor.tsx
+++ src/components/tiptap-templates/simple/simple-editor.tsx
@@ -1,88 +1,86 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { EditorContent, EditorContext, useEditor } from "@tiptap/react"
+import * as React from "react";
+import { EditorContent, EditorContext, useEditor } from "@tiptap/react";
 
 // --- Tiptap Core Extensions ---
-import { StarterKit } from "@tiptap/starter-kit"
-import { Image } from "@tiptap/extension-image"
-import { TaskItem } from "@tiptap/extension-task-item"
-import { TaskList } from "@tiptap/extension-task-list"
-import { TextAlign } from "@tiptap/extension-text-align"
-import { Typography } from "@tiptap/extension-typography"
-import { Highlight } from "@tiptap/extension-highlight"
-import { Subscript } from "@tiptap/extension-subscript"
-import { Superscript } from "@tiptap/extension-superscript"
-import { Underline } from "@tiptap/extension-underline"
+import { StarterKit } from "@tiptap/starter-kit";
+import { Image } from "@tiptap/extension-image";
+import { TaskItem } from "@tiptap/extension-task-item";
+import { TaskList } from "@tiptap/extension-task-list";
+import { TextAlign } from "@tiptap/extension-text-align";
+import { Typography } from "@tiptap/extension-typography";
+import { Highlight } from "@tiptap/extension-highlight";
+import { Underline } from "@tiptap/extension-underline";
 
 // --- Custom Extensions ---
-import { Link } from "@/components/tiptap-extension/link-extension"
-import { Selection } from "@/components/tiptap-extension/selection-extension"
-import { TrailingNode } from "@/components/tiptap-extension/trailing-node-extension"
+import { Link } from "@/components/tiptap-extension/link-extension";
+import { Selection } from "@/components/tiptap-extension/selection-extension";
+import { TrailingNode } from "@/components/tiptap-extension/trailing-node-extension";
 
 // --- UI Primitives ---
-import { Button } from "@/components/tiptap-ui-primitive/button"
-import { Spacer } from "@/components/tiptap-ui-primitive/spacer"
+import { Button } from "@/components/tiptap-ui-primitive/button";
+import { Spacer } from "@/components/tiptap-ui-primitive/spacer";
 import {
   Toolbar,
   ToolbarGroup,
   ToolbarSeparator,
-} from "@/components/tiptap-ui-primitive/toolbar"
+} from "@/components/tiptap-ui-primitive/toolbar";
 
 // --- Tiptap Node ---
-import { ImageUploadNode } from "@/components/tiptap-node/image-upload-node/image-upload-node-extension"
-import "@/components/tiptap-node/code-block-node/code-block-node.scss"
-import "@/components/tiptap-node/list-node/list-node.scss"
-import "@/components/tiptap-node/image-node/image-node.scss"
-import "@/components/tiptap-node/paragraph-node/paragraph-node.scss"
+import { ImageUploadNode } from "@/components/tiptap-node/image-upload-node/image-upload-node-extension";
+import "@/components/tiptap-node/code-block-node/code-block-node.scss";
+import "@/components/tiptap-node/list-node/list-node.scss";
+import "@/components/tiptap-node/image-node/image-node.scss";
+import "@/components/tiptap-node/paragraph-node/paragraph-node.scss";
 
 // --- Tiptap UI ---
-import { HeadingDropdownMenu } from "@/components/tiptap-ui/heading-dropdown-menu"
-import { ImageUploadButton } from "@/components/tiptap-ui/image-upload-button"
-import { ListDropdownMenu } from "@/components/tiptap-ui/list-dropdown-menu"
-import { NodeButton } from "@/components/tiptap-ui/node-button"
+import { HeadingDropdownMenu } from "@/components/tiptap-ui/heading-dropdown-menu";
+import { ImageUploadButton } from "@/components/tiptap-ui/image-upload-button";
+import { ListDropdownMenu } from "@/components/tiptap-ui/list-dropdown-menu";
+import { NodeButton } from "@/components/tiptap-ui/node-button";
 import {
   HighlightPopover,
   HighlightContent,
   HighlighterButton,
-} from "@/components/tiptap-ui/highlight-popover"
+} from "@/components/tiptap-ui/highlight-popover";
 import {
   LinkPopover,
   LinkContent,
   LinkButton,
-} from "@/components/tiptap-ui/link-popover"
-import { MarkButton } from "@/components/tiptap-ui/mark-button"
-import { TextAlignButton } from "@/components/tiptap-ui/text-align-button"
-import { UndoRedoButton } from "@/components/tiptap-ui/undo-redo-button"
+} from "@/components/tiptap-ui/link-popover";
+import { MarkButton } from "@/components/tiptap-ui/mark-button";
+import { TextAlignButton } from "@/components/tiptap-ui/text-align-button";
+import { UndoRedoButton } from "@/components/tiptap-ui/undo-redo-button";
 
 // --- Icons ---
-import { ArrowLeftIcon } from "@/components/tiptap-icons/arrow-left-icon"
-import { HighlighterIcon } from "@/components/tiptap-icons/highlighter-icon"
-import { LinkIcon } from "@/components/tiptap-icons/link-icon"
+import { ArrowLeftIcon } from "@/components/tiptap-icons/arrow-left-icon";
+import { HighlighterIcon } from "@/components/tiptap-icons/highlighter-icon";
+import { LinkIcon } from "@/components/tiptap-icons/link-icon";
 
 // --- Hooks ---
-import { useMobile } from "@/hooks/use-mobile"
-import { useWindowSize } from "@/hooks/use-window-size"
+import { useMobile } from "@/hooks/use-mobile";
+import { useWindowSize } from "@/hooks/use-window-size";
 
 // --- Components ---
-import { ThemeToggle } from "@/components/tiptap-templates/simple/theme-toggle"
+import { ThemeToggle } from "@/components/tiptap-templates/simple/theme-toggle";
 
 // --- Lib ---
-import { handleImageUpload, MAX_FILE_SIZE } from "@/lib/tiptap-utils"
+import { handleImageUpload, MAX_FILE_SIZE } from "@/lib/tiptap-utils";
 
 // --- Styles ---
-import "@/components/tiptap-templates/simple/simple-editor.scss"
+import "@/components/tiptap-templates/simple/simple-editor.scss";
 
-import content from "@/components/tiptap-templates/simple/data/content.json"
+import content from "@/components/tiptap-templates/simple/data/content.json";
 
 const MainToolbarContent = ({
   onHighlighterClick,
   onLinkClick,
   isMobile,
 }: {
-  onHighlighterClick: () => void
-  onLinkClick: () => void
-  isMobile: boolean
+  onHighlighterClick: () => void;
+  onLinkClick: () => void;
+  isMobile: boolean;
 }) => {
   return (
     <>
@@ -121,13 +119,6 @@ const MainToolbarContent = ({
       <ToolbarSeparator />
 
       <ToolbarGroup>
-        <MarkButton type="superscript" />
-        <MarkButton type="subscript" />
-      </ToolbarGroup>
-
-      <ToolbarSeparator />
-
-      <ToolbarGroup>
         <TextAlignButton align="left" />
         <TextAlignButton align="center" />
         <TextAlignButton align="right" />
@@ -148,15 +139,15 @@ const MainToolbarContent = ({
         <ThemeToggle />
       </ToolbarGroup>
     </>
-  )
-}
+  );
+};
 
 const MobileToolbarContent = ({
   type,
   onBack,
 }: {
-  type: "highlighter" | "link"
-  onBack: () => void
+  type: "highlighter" | "link";
+  onBack: () => void;
 }) => (
   <>
     <ToolbarGroup>
@@ -174,19 +165,19 @@ const MobileToolbarContent = ({
 
     {type === "highlighter" ? <HighlightContent /> : <LinkContent />}
   </>
-)
+);
 
 export function SimpleEditor() {
-  const isMobile = useMobile()
-  const windowSize = useWindowSize()
+  const isMobile = useMobile();
+  const windowSize = useWindowSize();
   const [mobileView, setMobileView] = React.useState<
     "main" | "highlighter" | "link"
-  >("main")
-  const [rect, setRect] = React.useState({ y: 0 })
+  >("main");
+  const [rect, setRect] = React.useState({ y: 0 });
 
   React.useEffect(() => {
-    setRect(document.body.getBoundingClientRect())
-  }, [])
+    setRect(document.body.getBoundingClientRect());
+  }, []);
 
   const editor = useEditor({
     immediatelyRender: false,
@@ -207,9 +198,6 @@ export function SimpleEditor() {
       Highlight.configure({ multicolor: true }),
       Image,
       Typography,
-      Superscript,
-      Subscript,
-
       Selection,
       ImageUploadNode.configure({
         accept: "image/*",
@@ -222,13 +210,13 @@ export function SimpleEditor() {
       Link.configure({ openOnClick: false }),
     ],
     content: content,
-  })
+  });
 
   React.useEffect(() => {
     if (!isMobile && mobileView !== "main") {
-      setMobileView("main")
+      setMobileView("main");
     }
-  }, [isMobile, mobileView])
+  }, [isMobile, mobileView]);
 
   return (
     <EditorContext.Provider value={{ editor }}>
@@ -263,5 +251,5 @@ export function SimpleEditor() {
         />
       </div>
     </EditorContext.Provider>
-  )
+  );
 }
--- src/components/tiptap-ui/mark-button/mark-button.tsx
+++ src/components/tiptap-ui/mark-button/mark-button.tsx
@@ -1,52 +1,43 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { isNodeSelection, type Editor } from "@tiptap/react"
+import * as React from "react";
+import { isNodeSelection, type Editor } from "@tiptap/react";
 
 // --- Hooks ---
-import { useTiptapEditor } from "@/hooks/use-tiptap-editor"
+import { useTiptapEditor } from "@/hooks/use-tiptap-editor";
 
 // --- Icons ---
-import { BoldIcon } from "@/components/tiptap-icons/bold-icon"
-import { Code2Icon } from "@/components/tiptap-icons/code2-icon"
-import { ItalicIcon } from "@/components/tiptap-icons/italic-icon"
-import { StrikeIcon } from "@/components/tiptap-icons/strike-icon"
-import { SubscriptIcon } from "@/components/tiptap-icons/subscript-icon"
-import { SuperscriptIcon } from "@/components/tiptap-icons/superscript-icon"
-import { UnderlineIcon } from "@/components/tiptap-icons/underline-icon"
+import { BoldIcon } from "@/components/tiptap-icons/bold-icon";
+import { Code2Icon } from "@/components/tiptap-icons/code2-icon";
+import { ItalicIcon } from "@/components/tiptap-icons/italic-icon";
+import { StrikeIcon } from "@/components/tiptap-icons/strike-icon";
+import { UnderlineIcon } from "@/components/tiptap-icons/underline-icon";
 
 // --- Lib ---
-import { isMarkInSchema } from "@/lib/tiptap-utils"
+import { isMarkInSchema } from "@/lib/tiptap-utils";
 
 // --- UI Primitives ---
-import { Button, ButtonProps } from "@/components/tiptap-ui-primitive/button"
+import { Button, ButtonProps } from "@/components/tiptap-ui-primitive/button";
 
-export type Mark =
-  | "bold"
-  | "italic"
-  | "strike"
-  | "code"
-  | "underline"
-  | "superscript"
-  | "subscript"
+export type Mark = "bold" | "italic" | "strike" | "code" | "underline";
 
 export interface MarkButtonProps extends Omit<ButtonProps, "type"> {
   /**
    * The type of mark to toggle
    */
-  type: Mark
+  type: Mark;
   /**
    * Optional editor instance. If not provided, will use editor from context
    */
-  editor?: Editor | null
+  editor?: Editor | null;
   /**
    * Display text for the button (optional)
    */
-  text?: string
+  text?: string;
   /**
    * Whether this button should be hidden when the mark is not available
    */
-  hideWhenUnavailable?: boolean
+  hideWhenUnavailable?: boolean;
 }
 
 export const markIcons = {
@@ -55,9 +46,7 @@ export const markIcons = {
   underline: UnderlineIcon,
   strike: StrikeIcon,
   code: Code2Icon,
-  superscript: SuperscriptIcon,
-  subscript: SubscriptIcon,
-}
+};
 
 export const markShortcutKeys: Partial<Record<Mark, string>> = {
   bold: "Ctrl-b",
@@ -65,28 +54,26 @@ export const markShortcutKeys: Partial<Record<Mark, string>> = {
   underline: "Ctrl-u",
   strike: "Ctrl-Shift-s",
   code: "Ctrl-e",
-  superscript: "Ctrl-.",
-  subscript: "Ctrl-,",
-}
+};
 
 export function canToggleMark(editor: Editor | null, type: Mark): boolean {
-  if (!editor) return false
+  if (!editor) return false;
 
   try {
-    return editor.can().toggleMark(type)
+    return editor.can().toggleMark(type);
   } catch {
-    return false
+    return false;
   }
 }
 
 export function isMarkActive(editor: Editor | null, type: Mark): boolean {
-  if (!editor) return false
-  return editor.isActive(type)
+  if (!editor) return false;
+  return editor.isActive(type);
 }
 
 export function toggleMark(editor: Editor | null, type: Mark): void {
-  if (!editor) return
-  editor.chain().focus().toggleMark(type).run()
+  if (!editor) return;
+  editor.chain().focus().toggleMark(type).run();
 }
 
 export function isMarkButtonDisabled(
@@ -94,23 +81,23 @@ export function isMarkButtonDisabled(
   type: Mark,
   userDisabled: boolean = false
 ): boolean {
-  if (!editor) return true
-  if (userDisabled) return true
-  if (editor.isActive("codeBlock")) return true
-  if (!canToggleMark(editor, type)) return true
-  return false
+  if (!editor) return true;
+  if (userDisabled) return true;
+  if (editor.isActive("codeBlock")) return true;
+  if (!canToggleMark(editor, type)) return true;
+  return false;
 }
 
 export function shouldShowMarkButton(params: {
-  editor: Editor | null
-  type: Mark
-  hideWhenUnavailable: boolean
-  markInSchema: boolean
+  editor: Editor | null;
+  type: Mark;
+  hideWhenUnavailable: boolean;
+  markInSchema: boolean;
 }): boolean {
-  const { editor, type, hideWhenUnavailable, markInSchema } = params
+  const { editor, type, hideWhenUnavailable, markInSchema } = params;
 
   if (!markInSchema) {
-    return false
+    return false;
   }
 
   if (hideWhenUnavailable) {
@@ -118,15 +105,15 @@ export function shouldShowMarkButton(params: {
       isNodeSelection(editor?.state.selection) ||
       !canToggleMark(editor, type)
     ) {
-      return false
+      return false;
     }
   }
 
-  return true
+  return true;
 }
 
 export function getFormattedMarkName(type: Mark): string {
-  return type.charAt(0).toUpperCase() + type.slice(1)
+  return type.charAt(0).toUpperCase() + type.slice(1);
 }
 
 export function useMarkState(
@@ -134,13 +121,13 @@ export function useMarkState(
   type: Mark,
   disabled: boolean = false
 ) {
-  const markInSchema = isMarkInSchema(type, editor)
-  const isDisabled = isMarkButtonDisabled(editor, type, disabled)
-  const isActive = isMarkActive(editor, type)
+  const markInSchema = isMarkInSchema(type, editor);
+  const isDisabled = isMarkButtonDisabled(editor, type, disabled);
+  const isActive = isMarkActive(editor, type);
 
-  const Icon = markIcons[type]
-  const shortcutKey = markShortcutKeys[type]
-  const formattedName = getFormattedMarkName(type)
+  const Icon = markIcons[type];
+  const shortcutKey = markShortcutKeys[type];
+  const formattedName = getFormattedMarkName(type);
 
   return {
     markInSchema,
@@ -149,7 +136,7 @@ export function useMarkState(
     Icon,
     shortcutKey,
     formattedName,
-  }
+  };
 }
 
 export const MarkButton = React.forwardRef<HTMLButtonElement, MarkButtonProps>(
@@ -167,7 +154,7 @@ export const MarkButton = React.forwardRef<HTMLButtonElement, MarkButtonProps>(
     },
     ref
   ) => {
-    const editor = useTiptapEditor(providedEditor)
+    const editor = useTiptapEditor(providedEditor);
 
     const {
       markInSchema,
@@ -176,18 +163,18 @@ export const MarkButton = React.forwardRef<HTMLButtonElement, MarkButtonProps>(
       Icon,
       shortcutKey,
       formattedName,
-    } = useMarkState(editor, type, disabled)
+    } = useMarkState(editor, type, disabled);
 
     const handleClick = React.useCallback(
       (e: React.MouseEvent<HTMLButtonElement>) => {
-        onClick?.(e)
+        onClick?.(e);
 
         if (!e.defaultPrevented && !isDisabled && editor) {
-          toggleMark(editor, type)
+          toggleMark(editor, type);
         }
       },
       [onClick, isDisabled, editor, type]
-    )
+    );
 
     const show = React.useMemo(() => {
       return shouldShowMarkButton({
@@ -195,11 +182,11 @@ export const MarkButton = React.forwardRef<HTMLButtonElement, MarkButtonProps>(
         type,
         hideWhenUnavailable,
         markInSchema,
-      })
-    }, [editor, type, hideWhenUnavailable, markInSchema])
+      });
+    }, [editor, type, hideWhenUnavailable, markInSchema]);
 
     if (!show || !editor || !editor.isEditable) {
-      return null
+      return null;
     }
 
     return (
@@ -227,10 +214,10 @@ export const MarkButton = React.forwardRef<HTMLButtonElement, MarkButtonProps>(
           </>
         )}
       </Button>
-    )
+    );
   }
-)
+);
 
-MarkButton.displayName = "MarkButton"
+MarkButton.displayName = "MarkButton";
 
-export default MarkButton
+export default MarkButton;
